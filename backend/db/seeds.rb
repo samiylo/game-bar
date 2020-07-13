6882cec52b6ec79a69a9a7088f19d8f30e36f8f1
samiylo <skryshu@gmail.com>
2020-07-13 14:16:41 -0500

    add Review Cont/Mod

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+g1 = Game.create(title: "Minecraft", console: "All Consoles", image: "https://cdn.vox-cdn.com/thumbor/5dsDXMRKEkkO33LydtdgP4-xSBo=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/15957232/0fe20042_0bb8_4781_82f4_7130f928b021.jpg")
+g2 = Game.create(title: "Grant Theft Auto", console: "PC", image: "https://vignette.wikia.nocookie.net/gtawiki/images/7/76/CoverArt-GTAV.png/revision/latest?cb=20130826184215")
+g3 = Game.create(title: "CS:GO", console: "PC", image: "https://signalscv.com/wp-content/uploads/2020/02/2-800x800.png")
+
+
