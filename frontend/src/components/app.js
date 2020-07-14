8c27d03685aacf9d9b86b0720611b3323f9f1060
samiylo <skryshu@gmail.com>
2020-07-14 12:45:44 -0500

    fixed components

---
+++ frontend/src/components/app.js
@@ -0,0 +1,7 @@
+
+// The app.js instantiates the Games Class
+class App {
+    constructor() {
+        this.games = new Games()
+    }
+}
---
+++ frontend/src/components/games.js
@@ -0,0 +1,5 @@
+class Games {
+    constructor() {
+
+    }
+}
