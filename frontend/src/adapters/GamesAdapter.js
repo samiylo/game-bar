641e1d17bc865010b6410869e6ee7d1aa699fb1d
samiylo <skryshu@gmail.com>
2020-07-14 12:39:35 -0500

    add Components

--- frontend/src/adapters/GamesAdapter.js
+++ frontend/src/adapters/GamesAdapter.js
@@ -2,6 +2,11 @@
 // The adapter talks to the API
 class GamesAdapter {
     constructor() {
+        this.baseUrl = 'http://localhost:3000/games'
+    }
 
+    getGames() {
+        return fetch(this.baseUrl)
+        .then(res => res.json())
     }
 }
