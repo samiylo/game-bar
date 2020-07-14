ae7b10c51c5fb670297f2d02eece9d25475417b1
samiylo <skryshu@gmail.com>
2020-07-14 12:54:36 -0500

    tether Script

--- frontend/src/components/games.js
+++ frontend/src/components/games.js
@@ -1,5 +1,13 @@
 class Games {
     constructor() {
+        this.games = []
+        this.adapter = new GamesAdapter()
+        // this.bindEventListeners()
+        this.fetchAndLoadGames()
+    }
 
+    fetchAndLoadGames() {
+        this.adapter.getGames()
+        .then(games => {console.log(games)})
     }
 }
