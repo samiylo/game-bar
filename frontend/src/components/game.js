4829490a6d2c0cf78a98ddf938577bdd19d9d74c
samiylo <skryshu@gmail.com>
2020-07-15 13:22:58 -0500

    add Reviews

--- frontend/src/components/game.js
+++ frontend/src/components/game.js
@@ -38,10 +38,56 @@ class Game {
         image.src = this.image
         gameBlock.appendChild(image)
 
+        // Creates the Game Inso Element Container
+        const gameInfo = document.createElement('div')
+        gameInfo.className  = 'game-info'
+        gameBlock.appendChild(gameInfo)
+
+        this.createInfo(gameInfo)
+
+        // Create the Review Element
+        const reviewInfo = document.createElement('div')
+        reviewInfo.className = "review-info"
+        gameBlock.appendChild(reviewInfo)
+
+        this.infoReview(reviewInfo)
+
     }
 
     newReviewForm(e) {
 
+    }
+
+    // Creates the current Review
+    createInfo(gameElement) {
+        const title = document.createElement('h3')
+        title.setAttribute("class", 'game-title')
+        title.innerHTML = this.title
+        gameElement.appendChild(title)
+
+        const console = document.createElement('h3')
+        console.setAttribute("class", 'game-console')
+        console.innerHTML = `Console: ${this.console}`
+        gameElement.appendChild(console)
+    }
+
+    infoReview(element) {
+        const reviewTitle = document.createElement('h4')
+        reviewTitle.setAttribute("class", 'review-header')
+        reviewTitle.innerHTML = 'What I think:'
+        element.appendChild(reviewTitle)
+
+        const reviews = document.createElement('div')
+        reviews.setAttribute("id", `review-${this.id}`)
+        element.appendChild(reviews)
+
+        // This will map through the games reviews and add to the review elements
+        reviews.innerHTML = this.reviews.map(review => this.reviewBody(review)).join('')
+    }
+
+    reviewBody(review){
+        console.log(review)
+        return `<p>${review.content}</p>`
     }
 
     deleteGame(id){
