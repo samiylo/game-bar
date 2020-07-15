adfe36fbda352f08e04c2a5048dca61efe38e713
samiylo <skryshu@gmail.com>
2020-07-15 12:26:49 -0500

    add Game Functions

---
+++ frontend/src/components/game.js
@@ -0,0 +1,13 @@
+class Game {
+    constructor(gameJSON) {
+        this.id = gameJSON.id
+        this.title = gameJSON.title
+        this.console = gameJSON.console
+        this.image = gameJSON.image
+        this.reviews = gameJSON.reviews
+    }
+
+    renderGameElement() {
+
+    }
+}
--- frontend/src/components/games.js
+++ frontend/src/components/games.js
@@ -6,9 +6,36 @@ class Games {
         this.fetchAndLoadGames()
     }
 
+    initBindingsAndEventListeners() {
+        this.newGameForm = document.getElementById('new-game-form')
+        this.newGameTitle = document.getElementById('new-game-title')
+        this.newGameConsole = document.getElementById('new-game-console')
+        this.newGameImage = document.getElementById('new-game-image')
+        this.newGameForm.addEventListener('submit', this.createGame.bind(this))
+        
+    }
+
+    createGame(e) {
+        e.preventDefault();
+        const titleValue = this.newGameTitle.value
+        const consoleValue = this.newGameConsole.value
+        const imageValue = this.newGameImage.value
+
+        this.adapter.createGame(titleValue, consoleValue, imageValue)
+        .then(game => {
+            const newGame = new Game(game)
+            this.games.push(newGame)
+            this.newGameTitle.value = ' '
+            this.newGameConsole.value = ' '
+            this.newGameImage.value = ' '
+            newGame.renderGameElement()
+        })
+    }
+
     fetchAndLoadGames() {
         this.adapter.getGames()
-        .then(games => {games.forEach(game => this.games.push(game))
+        .then(games => {games.forEach(game => this.games.push(new Game(game)))
+            console.log(this.games)
         })
         .then(() => {
             this.renderGames()
@@ -17,6 +44,9 @@ class Games {
 
     renderGames() {
         console.log(this.games)
-        // this.games.map(game => game.renderBookElement())
+        // this.games.map(game => game.renderGameElement())
+
+        const gamesContainer = document.getElementById('games-container')
+        gamesContainer
     }
 }
